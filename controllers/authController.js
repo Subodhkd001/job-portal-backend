9143814ec5db5c1b9ca914c9568fcb1dc435ecce
Subodhkd001 <subodhdashmukhe@gmail.com>
2024-06-13 20:05:57 +0530

    login api created

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,7 +12,7 @@ export const registerController = async (req, res, next) => {
       next("Email is required");
     }
     if (!password) {
-     next("Password is required and greaater than 6 character");
+      next("Password is required and greaater than 6 character");
     }
 
     const existingUser = await userModel.findOne({ email });
@@ -28,16 +28,52 @@ export const registerController = async (req, res, next) => {
     res.status(201).send({
       success: true,
       message: "User created successfully",
-      user:{
-        name:user.name,
-        lastName:user.lastName,
-        email:user.email,
-        location:user.location,
-
+      user: {
+        name: user.name,
+        lastName: user.lastName,
+        email: user.email,
+        location: user.location,
       },
-      token
+      token,
     });
   } catch (error) {
     next(error);
+  }
+};
+
+export const loginController = async (req, res,next) => {
+  try {
+    const { email, password } = req.body;
+
+    // validation
+    if (!email || !password) {
+      next("Please provide all fields");
+    }
+    // find user by email
+    const user = await userModel.findOne({ email }).select("+password")
+    if (!user) {
+      next("Invalid Username or password");
+    }
+    user.password = undefined;
+    // compare password
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      next("Invalid Username or password");
+    }
+    const token = user.createJWT();
+    res.status(200).send({
+      success: true,
+      message: "User login successfully",
+      user: {
+        name: user.name,
+        lastName: user.lastName,
+        email: user.email,
+        location: user.location,
+      },
+      token,
+    });
+
+  } catch (error) {
+
   }
 };
--- models/userModel.js
+++ models/userModel.js
@@ -1,45 +1,56 @@
-import mongoose from 'mongoose'
-import validator from 'validator'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 // schema
 
-const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true, "Name is required"]
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
     },
-    lastName:{
-        type:String,
+    lastName: {
+      type: String,
     },
-    email:{
-        type:String,
-        required:[true, "Email is required"],
-        unique:true,
-        validate:validator.isEmail
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      validate: validator.isEmail,
     },
-    password:{
-        type:String,
-        required:[true, "Password is required"],
-        minlength:[6, "Password must be 6 character long"],
-        select:true,
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be 6 character long"],
+      select: true,
     },
-    location:{
-        type:String,
-        default:"India"
-    }
-}, {timestamps: true})
+    location: {
+      type: String,
+      default: "India",
+    },
+  },
+  { timestamps: true }
+);
 
 // middlewares for bcrypt
-userSchema.pre('save', async function(){
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt)
-})
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// compare password
+userSchema.methods.comparePassword = async function (userPassword) {
+  const isMatch = await bcrypt.compare(userPassword, this.password);
+  return isMatch;
+};
 
 // JSON webtoken
-userSchema.methods.createJWT = function(){
-    return jwt.sign({userId:this._id}, process.env.JWT_SECRET, {expiresIn:process.env.JWT_LIFETIME})
-}
+userSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
 
-export default mongoose.model('User', userSchema)
+export default mongoose.model("User", userSchema);
