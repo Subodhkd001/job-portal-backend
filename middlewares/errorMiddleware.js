b3bfa3465adab8cef2e9ef4db2ec9e80d1777b0f
Subodhkd001 <subodhdashmukhe@gmail.com>
2024-06-13 18:18:13 +0530

    encryption or hashing performed

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -5,7 +5,7 @@ const errorMiddleware = (err, req, res, next) => {
   // res.status(500).send({
   //   success: false,
   //   message: "Something went wrong",
-  //   err
+  //   err 
   // });
 
   const defaultErrors = {
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import validator from 'validator'
+import bcrypt from 'bcryptjs'
 // schema
 
 const userSchema = new mongoose.Schema({
@@ -27,5 +28,10 @@ const userSchema = new mongoose.Schema({
     }
 }, {timestamps: true})
 
+// middlewares
+userSchema.pre('save', async function(){
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+})
 
 export default mongoose.model('User', userSchema)
